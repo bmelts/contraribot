05f69362b9bea44274711b2ad9a03078f5a2c3a7
Bill Meltsner <bill@meltsner.com>
2014-07-20 23:15:31 -0700

    bot works now yeah

--- bot.py
+++ bot.py
@@ -1,16 +1,86 @@
+# -*- coding: utf-8 -*-
+
 import os
-from twython import TwythonStreamer
+import random
+import re
+import signal
+import sys
+from threading import Timer
+from htmlentitydefs import name2codepoint
+import requests
+from twython import Twython, TwythonStreamer
+
+searches = ['i can','i will','i\'ll','i can\'t','i cannot','i won\'t']
+opposites = {
+    'can': 'CAN’T',
+    'will': 'WON’T',
+    'i\'ll': 'I WON’T',
+    'can\'t': 'CAN',
+    'can’t': 'CAN',
+    'cannot': 'CAN',
+    'can not': 'CAN',
+    'won\'t': 'WILL',
+    'won’t': 'WILL',
+    'will not': 'WILL'
+}
+search_regex = re.compile(r'(%s)\b' % '|'.join(sorted(searches, None, len, True)), re.IGNORECASE)
+opposite_regex = re.compile(r'\b(%s)\b' % '|'.join(sorted(opposites.keys(), None, len, True)), re.IGNORECASE)
+html_decode = re.compile('&(%s);' % '|'.join(name2codepoint))
+
+twitter = Twython(os.environ['APP_KEY'],
+                  os.environ['APP_SECRET'],
+                  os.environ['OAUTH_TOKEN'],
+                  os.environ['OAUTH_TOKEN_SECRET'])
 
 class ContraribotStreamer(TwythonStreamer):
+    tweet = None
+    tweet_count = 0
+    timer = None
+
     def on_success(self, data):
+        if 'entities' in data:
+            entities = data['entities']
+            # filter out tweets with urls, attached media, or @mentions - let's not spam
+            if (('urls' in entities and entities['urls'])
+                or ('media' in entities and entities['media'])
+                or ('user_mentions' in entities and entities['user_mentions'])):
+                return
+
         if 'text' in data:
-            print data['text'].encode('utf-8')
+            text = html_decode.sub(lambda m: unichr(name2codepoint[m.group(1)]), data['text']).encode('utf-8')
+            if search_regex.match(text):
+                text = opposite_regex.sub(lambda m: opposites[m.group(1).lower()], text)
+                if random.randint(0,self.tweet_count) == 0 and len(text) <= 140:
+                    self.tweet = text
+                self.tweet_count += 1
 
     def on_error(self, status_code, data):
         print status_code
 
-twitter = ContraribotStreamer(os.environ['APP_KEY'],
-                              os.environ['APP_SECRET'],
-                              os.environ['OAUTH_TOKEN'],
-                              os.environ['OAUTH_TOKEN_SECRET'])
-twitter.statuses.filter(track='poop')
+    def post_tweet(self):
+        if self.tweet:
+            twitter.update_status(status=self.tweet)
+        self.tweet = None
+        self.tweet_count = 0
+        self.timer = Timer(1 * 60, self.post_tweet)
+        self.timer.start()
+
+streamer = ContraribotStreamer(os.environ['APP_KEY'],
+                               os.environ['APP_SECRET'],
+                               os.environ['OAUTH_TOKEN'],
+                               os.environ['OAUTH_TOKEN_SECRET'])
+
+streamer.timer = Timer(10, streamer.post_tweet)
+streamer.timer.start()
+
+def sigint(signal, frame):
+    streamer.timer.cancel()
+    streamer.disconnect()
+
+signal.signal(signal.SIGINT, sigint)
+
+while True:
+    try:
+        streamer.statuses.filter(track=','.join(searches))
+    except requests.exceptions.ChunkedEncodingError:
+        print '***idgi***'
